f3b550f32209f3227cb11c11caa7567e8e9819e6
J-888 <josemigueltajuelo@gmail.com>
2022-03-25 21:43:09 +0100

    Mock up implementation of two methods

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-const { Telegraf } = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const { Telegraf } = require('telegraf')
 
-bot.command('start', ctx => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to the Code Capsules telegram bot.\nI echo messages. Please try it', {
-    })
-})
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('JayBot start'));
+bot.help((ctx) => ctx.reply('Ask J'));
+bot.command('demo', (ctx) => {
+	ctx.replyWithPhoto({ url: "https://i.ytimg.com/vi/528Iafd1QlU/hg" }, { caption: "cat photo" })
+	//ctx.replyWithPhoto({ source: Buffer.alloc(10) }, { caption: "cat photo" })	
+});
 
-// copy every message and send to the user
-bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message))
+bot.command('meme', (ctx) => {
 
-// Start webhook via launch method (preferred)
-bot.launch({
-    webhook: {
-      domain: 'https://nodejs-telegram-echobot-dzgubf.codecapsules.co.za',
-      port: process.env.PORT
-    }
-  })
-  
+	//console.log(ctx.message);
+	
+	if(!ctx.update.message.reply_to_message) return;
+
+	if(!ctx.update.message.reply_to_message.photo) return;
+
+	let photos = ctx.update.message.reply_to_message.photo;
+
+	let biggestIndex = photos.length - 1;
+	/*let biggestIndex = -1;
+	let biggestHeight = -1;
+	
+	for(let i=0; i<photos.length; ++i){
+		if(photos[i].height >= biggestHeight) {
+			biggestIndex = i;
+			biggestHeight = photos[i].height;
+		}
+	}*/
+	
+	ctx.telegram.getFileLink( photos[biggestIndex].file_id ).then(
+		(link)=>{
+			console.log(link);
+		}
+	);	
+
+	ctx.update.message.text.split('#');
+	
+	//ctx.replyWithPhoto({ source: Buffer.alloc(10) }, { caption: "cat photo" })	
+});
+
+bot.launch()
+
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
